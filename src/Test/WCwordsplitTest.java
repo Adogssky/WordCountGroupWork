8431b493b90e5740c1b0bad21ac45e3245014761
Adogssky <656874319@qq.com>
2018-04-08 15:11:20 +0800

    代码修改

--- src/Test/WCwordsplitTest.java
+++ src/Test/WCwordsplitTest.java
@@ -30,109 +30,169 @@ class WCwordsplitTest {
 	private String s20 ="dream-girl-ssss";
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+		public void test() {
+			fail("Not yet implemented");
+		}
 
-	@Test
-	public void testSplit(){
-		ArrayList<String> result1 = new ArrayList<String>();
-		result1.add("ab-d");
-		result1.add("table");
-		assertEquals(result1,Test1.split(s1));
+		@Test
+		public void testSplit1(){
+			ArrayList<String> result1 = new ArrayList<String>();
+			result1.add("ab-d");
+			result1.add("table");
+			assertEquals(result1,Test1.split(s1));
+		}
 		
-		ArrayList<String> result2 = new ArrayList<String>();
-		result2.add("ab");
-		result2.add("d");
-		result2.add("table");
-		assertEquals(result2,Test1.split(s2));
+		@Test
+		public void testSplit2(){
+			ArrayList<String> result2 = new ArrayList<String>();
+			result2.add("ab");
+			result2.add("d");
+			result2.add("table");
+			assertEquals(result2,Test1.split(s2));
+		}
 		
-		ArrayList<String> result3 = new ArrayList<String>();
-		result3.add("a");
-		result3.add("d");
-		assertEquals(result3,Test1.split(s3));
+		@Test
+		public void testSplit3(){
+			ArrayList<String> result3 = new ArrayList<String>();
+			result3.add("a");
+			result3.add("d");
+			assertEquals(result3,Test1.split(s3));
+		}
 		
-		ArrayList<String> result4 = new ArrayList<String>();
-		result4.add("a");
-		assertEquals(result4,Test1.split(s4));
+		@Test
+		public void testSplit4(){
+			ArrayList<String> result4 = new ArrayList<String>();
+			result4.add("a");
+			assertEquals(result4,Test1.split(s4));
+		}
 		
-		ArrayList<String> result5 = new ArrayList<String>();
-		result5.add("a");
-		result5.add("d");
-		assertEquals(result5,Test1.split(s5));
+		@Test
+		public void testSplit5(){
+			ArrayList<String> result5 = new ArrayList<String>();
+			result5.add("a");
+			result5.add("d");
+			assertEquals(result5,Test1.split(s5));
+		}
 		
-		ArrayList<String> result6 = new ArrayList<String>();
-		result6.add("a");
-		result6.add("d");
-		result6.add("da");
-		assertEquals(result6,Test1.split(s6));
+		@Test
+		public void testSplit6(){
+			ArrayList<String> result6 = new ArrayList<String>();
+			result6.add("a");
+			result6.add("d");
+			result6.add("da");
+			assertEquals(result6,Test1.split(s6));
+		}
 		
-		ArrayList<String> result7 = new ArrayList<String>();
-		result7.add("let");
-		result7.add("s");
-		assertEquals(result7,Test1.split(s7));
+		@Test
+		public void testSplit7(){
+			ArrayList<String> result7 = new ArrayList<String>();
+			result7.add("let");
+			result7.add("s");
+			assertEquals(result7,Test1.split(s7));
+		}
 		
-		ArrayList<String> result8 = new ArrayList<String>();
-		result8.add("a");
-		assertEquals(result8,Test1.split(s8));
+		@Test
+		public void testSplit8(){
+			ArrayList<String> result8 = new ArrayList<String>();
+			result8.add("a");
+			assertEquals(result8,Test1.split(s8));
+		}
 		
-		ArrayList<String> result9 = new ArrayList<String>();
-		result9.add("see");
-		result9.add("Box");
-		result9.add("d");
-		result9.add("c");
-		assertEquals(result9,Test1.split(s9));
+		@Test
+		public void testSplit9(){
+			ArrayList<String> result9 = new ArrayList<String>();
+			result9.add("see");
+			result9.add("Box");
+			result9.add("d");
+			result9.add("c");
+			assertEquals(result9,Test1.split(s9));
+		}
 		
-		ArrayList<String> result10 = new ArrayList<String>();
-		result10.add("a");
-		result10.add("d");
-		assertEquals(result10,Test1.split(s10));
+		@Test
+		public void testSplit10(){
+			ArrayList<String> result10 = new ArrayList<String>();
+			result10.add("a");
+			result10.add("d");
+			assertEquals(result10,Test1.split(s10));
+		}
 		
-		ArrayList<String> result11 = new ArrayList<String>();
-		result11.add("a");
-		result11.add("d");
-		assertEquals(result11,Test1.split(s11));
+		@Test
+		public void testSplit11(){
+			ArrayList<String> result11 = new ArrayList<String>();
+			result11.add("a");
+			result11.add("d");
+			assertEquals(result11,Test1.split(s11));
+			
+		}
 		
-		ArrayList<String> result12 = new ArrayList<String>();
-		result12.add("a");
-		result12.add("d");
-		assertEquals(result12,Test1.split(s12));
+		@Test
+		public void testSplit12(){
+			ArrayList<String> result12 = new ArrayList<String>();
+			result12.add("a");
+			result12.add("d");
+			assertEquals(result12,Test1.split(s12));
+		}
 		
-		ArrayList<String> result13 = new ArrayList<String>();
-		result13.add("software");
-		assertEquals(result13,Test1.split(s13));
+		@Test
+		public void testSplit13(){
+			ArrayList<String> result13 = new ArrayList<String>();
+			result13.add("software");
+			assertEquals(result13,Test1.split(s13));
+		}
 		
-		ArrayList<String> result14 = new ArrayList<String>();
-		result14.add("d");
-		assertEquals(result14,Test1.split(s14));
+		@Test
+		public void testSplit14(){
+			ArrayList<String> result14 = new ArrayList<String>();
+			result14.add("d");
+			assertEquals(result14,Test1.split(s14));
+		}
 		
-		ArrayList<String> result15 = new ArrayList<String>();
-		result15.add("d");
-		assertEquals(result15,Test1.split(s15));
+		@Test
+		public void testSplit15(){
+			ArrayList<String> result15 = new ArrayList<String>();
+			result15.add("d");
+			assertEquals(result15,Test1.split(s15));
+		}
 		
-		ArrayList<String> result16 = new ArrayList<String>();
-		result16.add("table");
-		result16.add("yuan");
-		assertEquals(result16,Test1.split(s16));
+		@Test
+		public void testSplit16(){
+			ArrayList<String> result16 = new ArrayList<String>();
+			result16.add("table");
+			result16.add("yuan");
+			assertEquals(result16,Test1.split(s16));
+		}
 		
-		ArrayList<String> result17 = new ArrayList<String>();
-		result17.add("love");
-		assertEquals(result17,Test1.split(s17));
+		@Test
+		public void testSplit17(){
+			ArrayList<String> result17 = new ArrayList<String>();
+			result17.add("love");
+			assertEquals(result17,Test1.split(s17));
+		}
 		
-		ArrayList<String> result18 = new ArrayList<String>();
-		result18.add("y");
-		result18.add("o");
-		result18.add("u");
-		assertEquals(result18,Test1.split(s18));
+		@Test
+		public void testSplit18(){
+			ArrayList<String> result18 = new ArrayList<String>();
+			result18.add("y");
+			result18.add("o");
+			result18.add("u");
+			assertEquals(result18,Test1.split(s18));
+		}
 		
-		ArrayList<String> result19 = new ArrayList<String>();
-		result19.add("d");
-		result19.add("a");
-		result19.add("d");
-		assertEquals(result19,Test1.split(s19));
+		@Test
+		public void testSplit19(){
+			ArrayList<String> result19 = new ArrayList<String>();
+			result19.add("d");
+			result19.add("a");
+			result19.add("d");
+			assertEquals(result19,Test1.split(s19));
+		}
 		
-		ArrayList<String> result20 = new ArrayList<String>();
-		result20.add("dream-girl-ssss");
-		assertEquals(result20,Test1.split(s20));
-	}
+		@Test
+		public void testSplit20(){
+			ArrayList<String> result20 = new ArrayList<String>();
+			result20.add("dream-girl-ssss");
+			assertEquals(result20,Test1.split(s20));
+		}
+		
+		
 }
